f4720828144fc11b06c092be2bc63f600e69fac2
Rasmi Jati <jatirasmi65@gmail.com>
2023-09-20 18:50:59 +0545

    created user model and meetingdbcontext class and installed required packages

---
+++ Data/MeetingDbContext.cs
@@ -0,0 +1,14 @@
+﻿using MeetingRoomSystem.Models.Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MeetingRoomSystem.Data
+{
+    public class MeetingDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public MeetingDbContext(DbContextOptions<MeetingDbContext> options) : base(options)
+        {
+            
+        }
+    }
+}
---
+++ Models/Domain/ApplicationUser.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace MeetingRoomSystem.Models.Domain
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
